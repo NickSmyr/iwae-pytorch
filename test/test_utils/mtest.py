4227a2c374f34af3927f8b8c2f1353b2b6060c17
Nick Smyr <smyrnioudis.nikolaos@outlook.com>
2022-01-02 11:45:02 +0100

    unit testing like scripts

--- test/test_utils/mtest.py
+++ test/test_utils/mtest.py
@@ -1,8 +1,14 @@
 import unittest
 
+
+from utils.data_download import download_data
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        self.assertEqual(True, False)
+        download_data("../../data")
+
+    def dataset_stats(self):
+        pass
+
 
 if __name__ == '__main__':
     unittest.main()
