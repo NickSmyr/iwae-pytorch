0baec85f67b54040d6086b55833a01a6641134c8
Emil Jansson <emijan@kth.se>
2022-01-04 14:41:07 +0100

    Some refactoring and improvement of calculation of log likelihood.

--- src/modules/validate.py
+++ src/modules/validate.py
@@ -3,14 +3,15 @@ import torch
 
 def validate(dataloader, model, device):
     loss = 0
+    num_data_points = 0
 
     with torch.no_grad():
         for X, y in dataloader:
+            num_data_points += len(X)
             X = X.to(device)
             objective = model.objective(X)
             loss += objective.item()
 
-    num_batches = len(dataloader)
-    loss /= num_batches
+    loss /= num_data_points
 
     return loss
