39be9269ebf21824b51d4511bae9bf14d5c8d262
Emil Jansson <emijan@kth.se>
2022-01-07 18:07:55 +0100

    Added experiement for plotting posteriors and reconstructed samples.

--- src/modules/experiments.py
+++ src/modules/experiments.py
@@ -3,7 +3,9 @@ import os
 import matplotlib.pyplot as plt
 
 import torch
+import numpy as np
 
+import dataloaders.mnist
 from ifaces import DownloadableDataset
 from modules.iwae import IWAE
 from modules.vae import VAE
@@ -63,6 +65,7 @@ def load_checkpoint(checkpoint_fname):
 
     return _model
 
+
 def load_and_plot_samples(checkpoint_path, num_samples):
     DownloadableDataset.set_data_directory('../data')
     model: VAE = load_checkpoint(checkpoint_path)
@@ -70,5 +73,176 @@ def load_and_plot_samples(checkpoint_path, num_samples):
     plt.imshow(res)
     plt.show()
 
+
+def calc_log_p(model, x_sample, minx, maxx, miny, maxy, num_points):
+    x = np.linspace(minx, maxx, num_points)
+    y = np.linspace(miny, maxy, num_points)
+
+    xg, yg = np.meshgrid(x, y)
+
+    xg1d = xg.reshape((-1, 1))
+    yg1d = yg.reshape((-1, 1))
+
+    log_p = np.empty((num_points, num_points))
+    for i in range(num_points):
+
+        h_samples_np = np.vstack((xg[i], yg[i])).T
+
+        with torch.no_grad():
+            h_samples = torch.from_numpy(h_samples_np).type(torch.get_default_dtype())
+            log_p_partial = model.calc_log_p(x_sample, h_samples).detach().numpy()
+            log_p[i,:] = log_p_partial
+
+    return x, y, log_p
+
+
+
+def true_posterior_find_box(model, x_sample, minx, maxx, miny, maxy, num_points, nll_range):
+
+    x, y, log_p = calc_log_p(model, x_sample, minx, maxx, miny, maxy, num_points)
+
+    max_nll = np.max(log_p)
+    max_index = np.unravel_index(np.argmax(log_p), log_p.shape)
+
+
+    # Disable code for finding the size of the interesting area in the latent space
+
+    # min_nll = max_nll - nll_range
+
+    # indices = np.where(log_p > min_nll)
+
+    # minxi = np.min(indices[1])
+    # maxxi = np.max(indices[1])
+    # minyi = np.min(indices[0])
+    # maxyi = np.max(indices[0])
+
+    # minx = x[minxi]
+    # maxx = x[maxxi]
+    # miny = y[minyi]
+    # maxy = y[maxyi]
+
+    # width = maxx - minx
+    # height = maxy - miny
+
+    # centerx = minx + 0.5 * width
+    # centery = miny + 0.5 * height
+
+    # size = 1.5 * max(width, height)
+
+    # size = min(2.0, size)
+
+
+    # Instead, always plot a square box of the same size in latent space
+    size = 1.0
+
+    centerx = x[max_index[1]]
+    centery = y[max_index[0]]
+
+
+    minx = centerx - 0.5 * size
+    maxx = centerx + 0.5 * size
+    miny = centery - 0.5 * size
+    maxy = centery + 0.5 * size
+
+    print(centerx, centery, size, minx, maxx, miny, maxy)
+
+    return minx, maxx, miny, maxy
+
+
+def generate_true_posterior_image(model, x_sample):
+    num_points = 300
+
+    search_minx = -5.0
+    search_maxx = 5.0
+    search_miny = -5.0
+    search_maxy = 5.0
+
+    nll_range = 10
+
+    minx, maxx, miny, maxy = true_posterior_find_box(model, x_sample, search_minx, search_maxx, search_miny, search_maxy, num_points, nll_range)
+
+    x, y, log_p = calc_log_p(model, x_sample, minx, maxx, miny, maxy, num_points)
+
+    max_nll = np.max(log_p)
+
+    adjusted_log_p = log_p - max_nll
+
+    # The structure of the posterior is easier to see with a smaller base
+
+    # image = np.exp(adjusted_log_p)
+    image = np.power(1.02, adjusted_log_p)
+
+    return image, minx, maxx, miny, maxy
+
+
+def generate_and_plot_posterior(model, x_sample):
+    image, minx, maxx, miny, maxy = generate_true_posterior_image(model, x_sample)
+    plt.imshow(-image, extent=[minx, maxx, miny, maxy], origin='lower', cmap="gray")
+
+
+def generate_and_plot_reconstructed_samples(model, x_sample):
+    img = np.zeros((0, 28))
+
+    for j in range(5):
+        pred = model(x_sample)
+        pred_img = pred.detach().squeeze().numpy().reshape((28, 28))
+        img = np.vstack((img, pred_img))
+
+    plt.xticks([])
+    plt.yticks([])
+    plt.imshow(-img, cmap="gray")
+
+
+def plot_true_posteriors_and_reconstructed_samples(checkpoint1_path, checkpoint2_path, figure_path):
+    DownloadableDataset.set_data_directory('../data')
+
+    test_dataloader = dataloaders.mnist.MnistDataloader(
+        train_not_test=False,
+        batch_size=400,
+        pin_memory=True,
+        shuffle=False)
+
+    model1: VAE = load_checkpoint(checkpoint1_path)
+    model2: IWAE = load_checkpoint(checkpoint2_path)
+
+    x_samples_indices = [3, 2, 1, 32, 4, 8]
+    x_samples = [torch.bernoulli(test_dataloader.dataset[i]) for i in x_samples_indices]
+
+    num_samples = len(x_samples)
+
+    plt.subplots(num_samples, 5, figsize=(10, 15))
+
+    plt.tight_layout()
+
+    for i, x_sample in enumerate(x_samples):
+        # Plot x sample
+        plt.subplot(num_samples, 5, i * 5 + 1) 
+        img = x_sample.detach().squeeze().numpy().reshape((28, 28))
+        plt.xticks([])
+        plt.yticks([])
+        plt.imshow(-img, cmap="gray")
+
+        # Model 1
+        plt.subplot(num_samples, 5, i * 5 + 2)
+        generate_and_plot_posterior(model1, x_sample)
+
+        plt.subplot(num_samples, 5, i * 5 + 3)
+        generate_and_plot_reconstructed_samples(model1, x_sample)
+
+        # Model 2
+        plt.subplot(num_samples, 5, i * 5 + 4)
+        generate_and_plot_posterior(model2, x_sample)
+
+        plt.subplot(num_samples, 5, i * 5 + 5)
+        generate_and_plot_reconstructed_samples(model2, x_sample)
+
+    plt.savefig(figure_path)
+
+
 if __name__ == '__main__':
-    load_and_plot_samples("../../checkpoints/mnist_iwae_k01_L2_bs100_final.pkl", 100)
+    # load_and_plot_samples("../../checkpoints/mnist_iwae_k01_L2_bs100_final.pkl", 100)
+
+    plot_true_posteriors_and_reconstructed_samples(
+        "../checkpoints/mnist_vae2d_k01_L1_bs400_final.pkl",
+        "../checkpoints/mnist_iwae2d_k50_L1_bs400_final.pkl",
+        "../figures/posteriors.pdf")
